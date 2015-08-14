a062c3dc5d930db93fc3937fc7d0de0c0e75a173
UltraKapes <kapes1980@hotmail.com>
2015-08-14 23:13:31 +1000

    travis test

--- tests/Database_Test.php
+++ tests/Database_Test.php
@@ -1,14 +1,14 @@
 <?php
 
-#require_once('../classes/Database.php');
+require_once('../classes/Database.php');
 
 class Database_Test extends PHPUnit_Framework_TestCase {
 
-  // public function testConnectionOk()
-  // {
-  //   $db = Database::getInstance();
-  //   $this->assertNotEmpty($db);
-  // }
+  public function testConnectionOk()
+  {
+    $db = Database::getInstance();
+    $this->assertNotEmpty($db);
+  }
 
   public function testOnePlus() {
 		$this->assertEquals(1+1,2);
