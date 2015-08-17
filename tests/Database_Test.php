d469553e095e2419d484d586e837a6bd85b5f50a
coderay1100 <rayp1100@gmail.com>
2015-08-17 15:55:22 +1000

    add some comments

--- tests/Database_Test.php
+++ tests/Database_Test.php
@@ -4,11 +4,12 @@ require_once('../classes/Database.php');
 
 class Database_Test extends PHPUnit_Framework_TestCase {
 
+  // Test if database object is instantiated
   public function testConnectionOk()
   {
     $db = Database::getInstance();
     $this->assertNotEmpty($db);
   }
 
-
+  
 }
