c775a61cca9f80d13d50e40eede1c62f27841281
UltraKapes <kapes1980@hotmail.com>
2015-08-26 22:35:12 +1000

    made picture as background, started to set up class for validation

--- index.php
+++ index.php
@@ -106,199 +106,147 @@
 
     
 </head>
-
 <body>
-    <style type="text/css">
-
-    .error {
-        color: red;
-    }
-
-    #signup_form label.error {
-        color: red;
-    }
-    
-    #signup_form input.error {
-        border: 1px solid red;
-    }
-    </style>
-    
-
     <div class="container-fluid3">
-    
         <nav class="navbar navbar-default navbar-static-top">
-        <div class="container-fluid">
-        <div class="navbar-header">
-        <a class="navbar-brand" href="#">
-       <img alt="Brand" src="...">
-       </a>
-       </div>
-          </div>
-        </nav>
-        
-        <section>
-        <div class="row">
-            <div id="banner" class="col-md-12 banner">
-                <img src="img/view.jpg">
-                <div class="text">
-                <p>The rental experience that actually works</p>
-                
-              </div>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">
+                        <img alt="Brand" src="...">
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="container">
-         <div class="row row-centered">
-        <div id="loginandsignup" class="col-md-4 col-centered ">
-
-        <form id="login" name="login" method="post" action="login.php">
-                    <!--<div class="input-group">-->
-                   
-                    <input name="username" type="text" id="username" class="form-control" placeholder="Username">
-
-                    <!--</div>-->
-                    <!--<div class="input-group">-->
-                    <input name="password" type="password" id="password" class="form-control " placeholder="Password">
-                    <!--</div>-->
-                    
-                    <div id="controlbuttons">
-                   <!--  <input class="btn btn-warning" type="reset" class="button" value="Reset"> -->
-                    <input type="submit" name="Submit" value="Login" id="login_btn" class="btn btn-custom custom"> 
-                    <br>
-                    <br>
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-custom custom" data-toggle="modal" data-target="#signup" onclick="clearForm()">
-                    Sign Up
-                </button>
-                </div>
-                 
-                </form>
-         </div>
-         </div>
-         </div>
-
-         </section>
-         <section>
-             <hr>
-             <div id="sectiondisplay" class="row">
-                <div class="col-md-4 icon">
-                <img height="100px" src="img/icon1.png">
-                <p>
-                here is some text talking about the icon
-                </p>
-                </div>
-                 <div class="col-md-4 icon">
-                 <img height="100px" src="img/icon2.png">
-                 <p>
-                here is some text talking about the icon
-                </p>
-                 </div>
-                 <div class="col-md-4 icon">
-                 <img height="100px" src="img/icon3.png">
-                 <p>
-                here is some text talking about the icon
-                </p>
-                 </div>
-             </div>
-         </section>
-          <section>
-             <hr>
-             <div id="sectiondisplay" class="row">
-                 <div class="col-md-6 icon">
-                 <img  src="img/logo.jpg">
-                 </div>
-                 <div class="col-md-4 icon ">
-                 <p>
-                 Franz Eilert, has been in the information technology field for most of his career. He sits on a few boards including the Spatial Information Business Association and is the director for Information Ecosystems.
-                 He is interested in this project as he sees the need to have some type of communication between all parties of the rental industry
-                 <br>
-                 <br>
-                Wall Fly is a repository of information where owners, agents and tenants can communicate and share important information.
-                Currently in the rental industry, all three parties are losing.
-                <br>
-                <br>
-                 The owner of a property has limited visibility of the property and related documents.
-                The agent is busy with a lot of properties and needs a streamline process to keep both owners and tenants content with the correct and up to date information.
-                The tenant has limited or nonexistent communication with all parties and does not have an easy documented outlet to ask for repairs.
-                </p>
-                 </div>
-             </div>
-         </section>
-         <section>
-             <hr>
-             <div id="sectiondisplay" class="row">
-                 <div class="col-md-12 ">
-                <p>
-                    Footer here
-                </p>
-                 </div>
-             </div>
-         </section>
-
-
-                
-                
-               
-                <!-- Modal -->
-                <div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                    <div class="modal-dialog modal-sm " role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                               <!--  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
-                                <h4 class="modal-title" id="myModalLabel">Wallfly Sign Up</h4>
-                            </div>
-                            <div class="modal-body">
-                                <form id="signup_form" name="signup_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                                    <!-- <label for="username">Username</label> -->
-                                    <input class="form-control" type="text" size="12" name="username" placeholder="Username" value="<?php echo $username ?>" id="usrname"/>
-                                    <span class="error"><?php echo $usernameErr;?></span>
+        </nav>
+        <section>
+            <div class="row">
+                <div class="col-md-12 banner">
+                    <div class="text">
+                        <p>The rental experience that actually works</p>
+                    </div>
+                    <div id="loginwrapper" class="col-md-3">
+                        <div id="loginandsignup" class="col-md-12 col-centered ">
+                            <form id="login" name="login" method="post" action="login.php">
+                                <!--<div class="input-group">-->
+                                <input name="username" type="text" id="username" class="form-control" placeholder="Username">
+                                <!--</div>-->
+                                <!--<div class="input-group">-->
+                                <input name="password" type="password" id="password" class="form-control " placeholder="Password">
+                                <!--</div>-->
+                                <div id="controlbuttons">
+                                    <!--  <input class="btn btn-warning" type="reset" class="button" value="Reset"> -->
+                                    <input type="submit" name="Submit" value="Login" id="login_btn" class="btn btn-custom custom">
                                     <br>
-                                    <!-- <label for="password">Password</label> -->
-                                    <input class="form-control" type="password" size="12" name="password" placeholder="Password" value="<?php echo $password ?>" id="psswrd"/>
-                                    <span class="error"><?php echo $passwordErr;?></span>
                                     <br>
-                                    <!-- <label for="first_name">First Name</label> -->
-                                    <input class="form-control" type='text' name='first_name' maxlength='50' size='30' placeholder='First Name' value="<?php echo $first_name ?>" id="fname"/>
-                                    <span class="error"><?php echo $first_nameErr;?></span>
-                                    <br>
-                                    <!-- <label for="last_name">Last Name</label> -->
-                                    <input class="form-control" type='text' name='last_name' maxlength='50' size='30' placeholder='Last Name' value="<?php echo $last_name ?>" id="lname"/>
-                                    <span class="error"><?php echo $last_nameErr;?></span>
-                                    <br>
-                                    <!-- <label for="email">Email Address</label> -->
-                                    <input class="form-control" type="text" name="email" maxlength="50" size="12" placeholder='Email Address' value="<?php echo $email ?>" id="email"/>
-                                    <span class="error"><?php echo $emailErr;?></span>
-                                    <br>
-                                    <!-- <label for="usertype">User Type</label> -->
-                                    <select class="form-control" name="usertype" placeholder='Please Select'>
-                                        <option value="">Please Select</option>
-                                        <option value="AGENT">Agent</option>
-                                        <option value="OWNER">Owner</option>
-                                        <option value="TENANT">Tenant</option>
-                                    </select>
-                                    <span class="error"><?php echo $usertypeErr;?></span>
-                                    <br>
-                            
-                            
-                                <input class="btn btn-success" type="submit" name="btnAdd" value="Add"> &nbsp;&nbsp;
-                                <input class="btn btn-warning" type="button" class="button" value="Reset" onclick="clearForm()"> &nbsp;&nbsp;
-                                <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="newPage()">Close</button>
-                                </form>
-                            </div>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-custom custom" data-toggle="modal" data-target="#signup" onclick="clearForm()">
+                                        Sign Up
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
+            </div>
+        </section>
+        <section>
             
-
-       
-        
-   
-
-
-
-       
-               
-           
+            <div id="sectiondisplay" class="row">
+                <div class="col-md-4 icon">
+                    <img height="100px" src="img/icon1.png">
+                    <p>
+                        here is some text talking about the icon
+                    </p>
+                </div>
+                <div class="col-md-4 icon">
+                    <img height="100px" src="img/icon2.png">
+                    <p>
+                        here is some text talking about the icon
+                    </p>
+                </div>
+                <div class="col-md-4 icon">
+                    <img height="100px" src="img/icon3.png">
+                    <p>
+                        here is some text talking about the icon
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <hr>
+            <div id="sectiondisplay" class="row">
+                <div class="col-md-6 icon">
+                    <img src="img/logo.jpg">
+                </div>
+                <div class="col-md-4 icon ">
+                    <p>
+                        Franz Eilert, has been in the information technology field for most of his career. He sits on a few boards including the Spatial Information Business Association and is the director for Information Ecosystems. He is interested in this project as he sees the need to have some type of communication between all parties of the rental industry
+                        <br>
+                        <br> Wall Fly is a repository of information where owners, agents and tenants can communicate and share important information. Currently in the rental industry, all three parties are losing.
+                        <br>
+                        <br> The owner of a property has limited visibility of the property and related documents. The agent is busy with a lot of properties and needs a streamline process to keep both owners and tenants content with the correct and up to date information. The tenant has limited or nonexistent communication with all parties and does not have an easy documented outlet to ask for repairs.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <hr>
+            <div id="sectiondisplay" class="row">
+                <div class="col-md-12 ">
+                    <p>
+                        Footer here
+                    </p>
+                </div>
+            </div>
+        </section>
+        <!-- Modal -->
+        <div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog modal-sm " role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="myModalLabel">Wallfly Sign Up</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form id="signup_form" name="signup_form" method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
+                            <!-- <label for="username">Username</label> -->
+                            <input class="form-control" type="text" size="12" name="username" placeholder="Username" value="<?php echo $username ?>" id="usrname" />
+                            <span class="error"><?php echo $usernameErr;?></span>
+                            <br>
+                            <!-- <label for="password">Password</label> -->
+                            <input class="form-control" type="password" size="12" name="password" placeholder="Password" value="<?php echo $password ?>" id="psswrd" />
+                            <span class="error"><?php echo $passwordErr;?></span>
+                            <br>
+                            <!-- <label for="first_name">First Name</label> -->
+                            <input class="form-control" type='text' name='first_name' maxlength='50' size='30' placeholder='First Name' value="<?php echo $first_name ?>" id="fname" />
+                            <span class="error"><?php echo $first_nameErr;?></span>
+                            <br>
+                            <!-- <label for="last_name">Last Name</label> -->
+                            <input class="form-control" type='text' name='last_name' maxlength='50' size='30' placeholder='Last Name' value="<?php echo $last_name ?>" id="lname" />
+                            <span class="error"><?php echo $last_nameErr;?></span>
+                            <br>
+                            <!-- <label for="email">Email Address</label> -->
+                            <input class="form-control" type="text" name="email" maxlength="50" size="12" placeholder='Email Address' value="<?php echo $email ?>" id="email" />
+                            <span class="error"><?php echo $emailErr;?></span>
+                            <br>
+                            <!-- <label for="usertype">User Type</label> -->
+                            <select class="form-control" name="usertype" placeholder='Please Select'>
+                                <option value="">Please Select</option>
+                                <option value="AGENT">Agent</option>
+                                <option value="OWNER">Owner</option>
+                                <option value="TENANT">Tenant</option>
+                            </select>
+                            <span class="error"><?php echo $usertypeErr;?></span>
+                            <br>
+                            <input class="btn btn-success" type="submit" name="btnAdd" value="Add"> &nbsp;&nbsp;
+                            <input class="btn btn-warning" type="button" class="button" value="Reset" onclick="clearForm()"> &nbsp;&nbsp;
+                            <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="newPage()">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 
     <?php
         if($validform == false) {
@@ -358,6 +306,7 @@
         
     ?>
 
+ 
 
 </body>
 
