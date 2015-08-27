af1b02dc171f54b2b9d0e020a188f0342b56e1e8
UltraKapes <kapes1980@hotmail.com>
2015-08-27 23:50:53 +1000

    merged kapes branch , turned back on jsvalidator

--- index.php
+++ index.php
@@ -44,8 +44,8 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <!-- JQuery Validate Plugin-->
-    <!--<script src="http://cdn.jsdelivr.net/jquery.validation/1.14.0/jquery.validate.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/additional-methods.js"></script>-->
+    <script src="http://cdn.jsdelivr.net/jquery.validation/1.14.0/jquery.validate.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/additional-methods.js"></script>
     <script type="text/javascript" src="js/index.js"></script>
 
 
