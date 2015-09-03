b520f9d9c5c11a933aaa053cbb0ab7155d08934a
Khalfan AlFalasi <AlFalasi95@d-i89-238-84.student.eduroam.uq.edu.au>
2015-09-03 11:10:16 +1000

    updated home.php style

--- home.php
+++ home.php
@@ -7,26 +7,30 @@
 
 ?>
 
-<!DOCTYPE HTML>
-<html>
-	<head lang="en">
-		<meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Wall Fly</title>
-		<!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <meta name="description" content="WallFly - Property Mangement System">
+    <meta name="author" content="The SuperFlyz">
+
+    <title>Welcome To WallFly</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="style/style.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link href="css/wallfly.css" rel="stylesheet">
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
 
-	</head>
-
-	<body>
+<body id="page-top" class="index">
     <!-- Navigator-->
     <nav class="navbar navbar-default">
       <div class="container-fluid">
