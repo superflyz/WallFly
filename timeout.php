6dc658ab4dcf0f900c9c1e70c03809ed63b3ef18
Shane Capon <kapes1980@hotmail.com>
2015-09-10 12:55:34 +1000

    timeout page formatted pdo style yo !

--- timeout.php
+++ timeout.php
@@ -2,8 +2,8 @@
 session_start();
  require_once(__DIR__.'/logincheck.php');
  require_once(__DIR__.'/classes/Database.php');
-$check_user = $_POST['username'];
-$check_password = $_POST['password'];
+$checkUser = $_POST['username'];
+$checkPassword = $_POST['password'];
 
 
  try{
@@ -16,38 +16,35 @@ $check_password = $_POST['password'];
 
 try{
 
-	//To protect MySQL injection
-	$check_user = stripslashes($check_user);
-	$check_password = stripcslashes($check_password);
-	$check_user = mysql_real_escape_string($check_user);
-	$check_password = mysql_real_escape_string($check_password);
 
 	//execute the SQL query and return records
-	$STH = $DBH->query("SELECT * FROM user WHERE username = '$check_user' and password = '$check_password'");
-	$STH->setFetchMode(PDO::FETCH_OBJ);
-
-	//Mysql_num_row is counting table row
-	$count = mysql_num_rows($result);
+	$statement = $DBH->prepare("SELECT * FROM user WHERE username=:checkUser and password=:checkPassword");
+	$statement->execute(array(':$checkUser'=>$checkUser,':$checkPassword'=>$checkPassword));
+	$statement ->setFetchMode(PDO::FETCH_OBJ);
 
 	//If result matched $check_user and $check_password, table row must be 1 row
-	if($STH->rowCount() == 1) {
-		$row = $STH->fetch();
+	if($statement->rowCount() == 1) {
+		$row = $statement->fetch();
 		//session expire setup
 		$_SESSION["expiration"] = time() + 1800;
 		
 		//session user setup
 		$_SESSION["usertype"] = $row->privilege;
 		$_SESSION["username"] = $row->username;
+		//close database
+		$DBH = NULL;
 		header("location:home.php");
 		exit();
 	}else{
+		//close database
+		$DBH = NULL;
 		header("Location:timedout.php");
+
 		exit();
 		
 	}
 
-	//close database
-	$DBH = NULL;
+
 
 }catch(PDOException $e) {
         echo "Problem logging in";
