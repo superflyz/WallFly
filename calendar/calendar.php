b7966236db8ed2c730f4ddebf61cb8dea0e79e77
UltraKapes <kapes1980@hotmail.com>
2015-09-12 16:51:15 +1000

    fixed timeout to include hashing, added some calendar image

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -1,11 +1,20 @@
+<?php
+session_start();
+require_once(__DIR__.'/../logincheck.php');
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
+    <title>Wallfly Calendar</title>
     <meta charset="utf-8"/>
-    <title>DZS Calendar - Preview Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="WallFly - Property Mangement System">
+    <meta name="author" content="The SuperFlyz">
     <link href="./bootstrap/bootstrap.css" rel="stylesheet">
+<!--    <link href="../css/bootstrap.min.css" rel="stylesheet">-->
+
     <link rel='stylesheet' type="text/css" href="style/style.css"/>
     <script src="js/jquery.js" type="text/javascript"></script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -18,6 +27,7 @@
     <link rel='stylesheet' type="text/css" href="dzscalendar/dzscalendar.css"/>
     <script src="dzscalendar/dzscalendar.js" type="text/javascript"></script>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<!--    <link href="../css/wallfly.css" rel="stylesheet">-->
 </head>
 
 <body>
@@ -28,24 +38,24 @@
             <div class="col-md-12">
                 <div class="dzscalendar skin-responsive-galileo" id="cal-responsive-galileo2" style="">
                     <div class="events">
-                        <div class="event-tobe" data-repeat="everymonth" data-day="09">
+                        <div class="event-tobe" data-repeat="everymonth" data-day="09" data-tag="blue" data-eventbg="">
                             <div style="width:200px;">
-                                <h5>On the prowl</h5>
-                                <span class="label">Date</span> 11-8-2012 at 6PM
+                                <h5>Inspection</h5>
+                                <span class="label">Time:</span> 10:00AM
                                 <br/>
-                                <span class="label">Location</span> UQ, St.Lucia
+                                <span class="label">Agent:</span> Gemma Chan
                                 <br/>
-                                <br/>Kartik is meeting with the Samalaiṅgika laṛakē near the lakes.
+                                <br/>Please be home or notify Ray White asap to organise alternate arrangements
                             </div>
                         </div>
-                        <div class="event-tobe" data-repeat="everymonth" data-day="18">
+                        <div class="event-tobe"  data-day="18" data-month="9" data-year="2015" data-tag="blue" data-eventbg="">
                             <div style="width:200px;">
-                                <h5>Special Alone time</h5>
-                                <span class="label">Date</span> 11-8-2012 at 6PM
+                                <h5>Maintenace Repair</h5>
+                                <span class="label">Time:</span>12:00PM
                                 <br/>
-                                <span class="label">Location</span> Kartik's House
+                                <span class="label">Contractor:</span> Mike's Plumbing
                                 <br/>
-                                <br/>Kartik is watching Samalaiṅgika aślīla
+                                <br/>Fixing leaking tap
                             </div>
                         </div>
                     </div>
@@ -58,7 +68,7 @@
 <script>
     jQuery(document).ready(function ($) {
         // cover pictures for calendar jan->dec in order
-        var design_month_covers = ['img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg', 'img/kartik.jpg'];
+        var design_month_covers = ['img/jan.jpg', 'img/feb.jpg', 'img/mar.jpg', 'img/apr.jpg', 'img/may.jpg', 'img/jun.jpg', 'img/jul.jpg', 'img/aug.jpg', 'img/sep.jpg', 'img/oct.jpg', 'img/nov.jpg', 'img/dec.jpg'];
 
         //set cover pictures
         dzscal_init("#cal-responsive-galileo2", {
--- timeout.php
+++ timeout.php
@@ -1,53 +1,55 @@
 <?php
 session_start();
- require_once(__DIR__.'/logincheck.php');
- require_once(__DIR__.'/classes/Database.php');
+require_once(__DIR__ . '/logincheck.php');
+require_once(__DIR__ . '/classes/Database.php');
 $checkUser = $_POST['username'];
 $checkPassword = $_POST['password'];
 
 
- try{
-        $DBH = Database::getInstance();
-        $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }catch(PDOException $e) {
-        echo "Unable to connect";
-        file_put_contents('Log/PDOErrorLog.txt', $e->getMessage(), FILE_APPEND);
+try {
+    $DBH = Database::getInstance();
+    $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Unable to connect";
+    file_put_contents('Log/PDOErrorLog.txt', $e->getMessage(), FILE_APPEND);
+}
+
+
+try {
+
+    $securePass = new SecurePassword;
+    //execute the SQL query and return records
+    $statement = $DBH->prepare("SELECT * FROM user WHERE username=:username");
+    $statement->execute(['username' => $checkUser]);
+    $result = $statement->fetch(PDO::FETCH_OBJ);
+
+    if ($result) {
+        $comparehash = $securePass->validate_password($checkPassword, $result->password);
+
+        if ($comparehash) {
+            //session expire setup
+            $_SESSION["expiration"] = time() + 1800;
+
+            //session user setup
+            $_SESSION["usertype"] = $result->privilege;
+            $_SESSION["username"] = $result->username;
+            //close database
+            $DBH = NULL;
+            header("location:home.php");
+            exit();
+        } else {
+            //close database
+            $DBH = NULL;
+            header("Location:timedout.php");
+            exit();
+        }
     }
 
-try{
 
-
-	//execute the SQL query and return records
-	$statement = $DBH->prepare("SELECT * FROM user WHERE username=:checkUser and password=:checkPassword");
-	$statement->execute(array(':$checkUser'=>$checkUser,':$checkPassword'=>$checkPassword));
-	$statement ->setFetchMode(PDO::FETCH_OBJ);
-
-	//If result matched $check_user and $check_password, table row must be 1 row
-	if($statement->rowCount() == 1) {
-		$row = $statement->fetch();
-		//session expire setup
-		$_SESSION["expiration"] = time() + 1800;
-		
-		//session user setup
-		$_SESSION["usertype"] = $row->privilege;
-		$_SESSION["username"] = $row->username;
-		//close database
-		$DBH = NULL;
-		header("location:home.php");
-		exit();
-	}else{
-		//close database
-		$DBH = NULL;
-		header("Location:timedout.php");
-
-		exit();
-		
-	}
-
-
-
-}catch(PDOException $e) {
-        echo "Problem logging in";
-        file_put_contents('Log/PDOErrorLog.txt', $e->getMessage(), FILE_APPEND);
-    }
+} catch (PDOException $e) {
+    //close database
+    $DBH = NULL;
+    echo "Problem logging in";
+    file_put_contents('Log/PDOErrorLog.txt', $e->getMessage(), FILE_APPEND);
+}
 ?>
